dc834a9720397ef2b89fdc0ccc969b2e2ef96fdc
Michael Dijkstra <micdijkstra@gmail.com>
2015-01-07 11:31:53 +1100

    Horizontal Drag, Scroll each Vertically

--- _scripts/scripts.coffee
+++ _scripts/scripts.coffee
@@ -6,8 +6,24 @@ setupCollection = ->
   $('.collection-right').css
     left: windowWidth - 100
 
-  $('.collection-wrapper').scroll (event) ->
+  $('.collection-wrapper').on "DOMMouseScroll mousewheel", (event) ->
     console.log event
+    if $('.collection-left').is(":hover")
+      $element = $('.collection-left')
+    else
+      $element = $('.collection-right')
+
+    $element.scrollTop $element.scrollTop() - event.originalEvent.wheelDeltaY
+    if event.originalEvent.detail > 0 or event.originalEvent.wheelDelta < 0 #alternative options for wheelData: wheelDeltaX & wheelDeltaY
+      #scroll down
+      console.log "Down"
+    else
+      #scroll up
+      $element.scrollTop $element.scrollTop() - 15
+      console.log "Up"
+    #prevent page fom scrolling
+    false
+
 
   $('.collection-wrapper').overscroll
    scrollLeft: collectionScrollLeft
--- site/assets/application.js
+++ site/assets/application.js
@@ -804,8 +804,22 @@
     $('.collection-right').css({
       left: windowWidth - 100
     });
-    $('.collection-wrapper').scroll(function(event) {
-      return console.log(event);
+    $('.collection-wrapper').on("DOMMouseScroll mousewheel", function(event) {
+      var $element;
+      console.log(event);
+      if ($('.collection-left').is(":hover")) {
+        $element = $('.collection-left');
+      } else {
+        $element = $('.collection-right');
+      }
+      $element.scrollTop($element.scrollTop() - event.originalEvent.wheelDeltaY);
+      if (event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0) {
+        console.log("Down");
+      } else {
+        $element.scrollTop($element.scrollTop() - 15);
+        console.log("Up");
+      }
+      return false;
     });
     return $('.collection-wrapper').overscroll({
       scrollLeft: collectionScrollLeft,
