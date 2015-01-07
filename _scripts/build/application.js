b329878b00af31ea46e559fb1e52f66e63935f33
Michael Dijkstra <micdijkstra@gmail.com>
2015-01-07 12:00:37 +1100

    Drag and scroll everywhere!

--- _scripts/build/application.js
+++ _scripts/build/application.js
@@ -15,20 +15,7 @@
     $('.collection-right').css({
       left: windowWidth - 200
     });
-    $('.collection-wrapper').on("DOMMouseScroll mousewheel", function(event) {
-      var $element;
-      console.log(event);
-      if ($('.collection-left').is(":hover")) {
-        $element = $('.collection-left');
-      } else {
-        $element = $('.collection-right');
-      }
-      $element.scrollTop($element.scrollTop() - event.originalEvent.wheelDeltaY);
-      $element.find('.collection-toggle').css({
-        top: $element.scrollTop() + 100
-      });
-      return false;
-    });
+    $('.collection-wrapper').height($('.collection-right').height());
     $('[data-collection-toggle]').click(function() {
       var factor, position;
       factor = parseInt($(this).attr('data-collection-toggle'));
--- _scripts/scripts.coffee
+++ _scripts/scripts.coffee
@@ -7,20 +7,11 @@ setupCollection = ->
   collectionScrollLeft = windowWidth/2 - 200
 
   $('.collection-container').width(windowWidth - 200)
+
   $('.collection-right').css
     left: windowWidth - 200
 
-  $('.collection-wrapper').on "DOMMouseScroll mousewheel", (event) ->
-    console.log event
-    if $('.collection-left').is(":hover")
-      $element = $('.collection-left')
-    else
-      $element = $('.collection-right')
-
-    $element.scrollTop $element.scrollTop() - event.originalEvent.wheelDeltaY
-    $element.find('.collection-toggle').css
-      top: $element.scrollTop() + 100
-    false
+  $('.collection-wrapper').height $('.collection-right').height()
 
   $('[data-collection-toggle]').click ->
     factor = parseInt($(this).attr('data-collection-toggle'))
